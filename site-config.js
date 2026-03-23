c7fabcdf72bf258cde4732afb4d850e14d7c6446
PIXORA24 <pixora24@gmail.com>
2026-03-23 08:39:56 +0530

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,57 @@
+// Generated from site-data.json via build-site.ps1
+window.SITE_CONFIG = {
+    "site":  {
+                 "baseUrl":  "https://pixora24.github.io/Vikesh-Prathiksha-Eng/",
+                 "pageTitle":  "Vikesh Rao \u0026 Pratiksha Rao Engagement Invitation",
+                 "landing":  {
+                                 "title":  "Vikesh Rao \u0026 Pratiksha Rao Engagement Invitation",
+                                 "description":  "Hey! With lots of love, we’re excited to invite you to our engagement 💍 It would mean a lot to have you with us on our special day. Please find the invite attached ✨ - Vikesh \u0026 Pratiksha",
+                                 "twitterTitle":  "Vikesh Rao \u0026 Pratiksha Rao Engagement Invitation",
+                                 "twitterDescription":  "Hey! With lots of love, we’re excited to invite you to our engagement 💍 It would mean a lot to have you with us on our special day. Please find the invite attached ✨ - Vikesh \u0026 Pratiksha",
+                                 "image":  "assets/previews/site-preview.webp"
+                             }
+             },
+    "studio":  {
+                   "enabled":  true,
+                   "name":  "Pixora24",
+                   "label":  "Crafted by",
+                   "url":  "https://www.instagram.com/pixora24?igsh=eWRldmc2emluNzU="
+               },
+    "events":  [
+                   {
+                       "key":  "engagement",
+                       "title":  "Engagement",
+                       "pageTitle":  "Vikesh Rao \u0026 Pratiksha Rao Engagement",
+                       "cardLabel":  "Engagement",
+                       "cardAlt":  "Engagement Invite",
+                       "cardImage":  "assets/engagement/thumb.webp",
+                       "labelClass":  "",
+                       "landingPage":  "engagement.html",
+                       "sharePages":  [
+
+                                      ],
+                       "share":  {
+                                     "title":  "Vikesh Rao \u0026 Pratiksha Rao Engagement Invitation",
+                                     "description":  "5 April 2026 | 10:00 AM | Lion’s Club Hall, Surathkal, XQVX+2J3, Surathkal, Karnataka 575014",
+                                     "image":  "assets/previews/engagement-preview.webp"
+                                 },
+                       "media":  {
+                                     "video":  "assets/engagement/video.mp4",
+                                     "audio":  "assets/engagement/music.mp3",
+                                     "poster":  "assets/engagement/bg.webp"
+                                 },
+                       "calendarTitle":  "Vikesh Rao \u0026 Pratiksha Rao Engagement",
+                       "calendarDurationHours":  2,
+                       "startDate":  "2026-04-05T10:00:00+05:30",
+                       "venue":  "Lion’s Club Hall, Surathkal, XQVX+2J3, Surathkal, Karnataka 575014",
+                       "mapQuery":  "Lion’s Club Hall, Surathkal, XQVX+2J3, Surathkal, Karnataka 575014",
+                       "directionsEnabled":  true,
+                       "calendarEnabled":  true,
+                       "countdownEnabled":  true,
+                       "location":  {
+                                        "lat":  "12.992594133866325",
+                                        "lng":  "74.79902290991812"
+                                    }
+                   }
+               ]
+};
